738cc7f261f73d8d12555dece35abe04b8dd18b8
VerityWoodcock <vmhknowles@gmail.com>
2023-04-20 10:05:12 +0000

    Quiz questions & recommendations
    Two new pages created - quiz.js and results.js in which I've typed up some of the content and included images. Just a skeleton. Typed on separate pages to make it easy to delete!

---
+++ our-app/src/quiz.js
@@ -0,0 +1,31 @@
+
+    {/* user takes the quiz, questions are here*/}
+    <>
+    <h3 className="questionNumber">Question 1</h3>
+    <p className="questionPhrase">Do any of the following apply to you?</p>
+    <p className="answer">I have a food allergy / intolerance</p>
+    <p className="answer">I am diabetic</p>
+    <p className="answer">I'm watching my weight</p>
+   
+    <h3 className="questionNumber">Question 2</h3>
+    <p className="questionPhrase">Which of these pictures inspires your tastebuds? <span italics>Click all that apply</span></p>
+    <img src="Images\Q2\Q2image1.png" alt="milk tea"/>
+    <img src="Images\Q2\Q2image2.png" alt="fruit tea"/>
+    <img src="Images\Q2\Q2image3.png" alt="brown sugar tea"/>
+    <img src="Images\Q2\Q2image4.png" alt="vegetable tea"/>
+    <img src="Images\Q2\Q2image5.png" alt="camomile tea"/>
+
+    <h3 className="questionNumber">Question 3</h3>
+    <p className="questionPhrase">Which of these chocolates would you prefer to receive?</p>
+    <img src="Images\Q3\Q3image1.png" alt="exuberantly fruity"/>
+    <img src="Images\Q3\Q3image2.png" alt="milk to caramel"/>
+    <img src="Images\Q3\Q3image3.png" alt="patisserie"/>
+    <img src="Images\Q3\Q3image4.png" alt="everything"/>
+    <img src="Images\Q3\Q3image5.png" alt="serious dark"/>
+    <img src="Images\Q3\Q3image6.png" alt="tipsy truffles"/>
+
+    <h3 className="questionNumber">Question 4</h3>
+    <p className="questionPhrase">Choose your preferred sugar level?</p>
+    <img src="Images\Q4\sugarLevel.png" alt="sugar meter"/>
+    </>
+
---
+++ our-app/src/results.js
@@ -0,0 +1,31 @@
+{/* after the quiz the user ends up with this page which recommends the tea, has a picture of all teas, 
+emphasises their specific tea and then tells them why they would have this tea*/}
+
+<header>
+
+
+<h3>Wow, great choice... you got...</h3>
+
+{/*images of teas, need to add an arrow or highlighting to emphasise the specific tea*/}
+<img src="Images\teas\teas.png" alt="allTeas" />
+
+<h1 className="recommendedTea">Milk tea</h1>
+<p className="whyThisTea">We would recommend our milk tea. Enjoy a taste of this good old reliable, knowing that it will be exactly what you expected. Please be aware that we add <emphasis>MILK</emphasis> to every cup and that this helps keep your calcium levels up.</p>
+
+<h1 className="recommendedTea">Fruit tea</h1>
+<p className="whyThisTea">We would recommend our fruit tea. Of course you’re sweet enough and this is the perfect combination of sweetness and freshness. Enjoy the extra vitamins we add to every cup to ensure you’re at your peak!</p>
+
+<h1 className="recommendedTea">Brown Sugar tea</h1>
+<p className="whyThisTea">We would recommend our brown sugar tea. Please be aware we add lots of <emphasis>SUGAR</emphasis> to every cup, which is fantastic when you need that sudden boost of energy as a pick me up.</p>
+
+<h1 className="recommendedTea">Vegetable tea</h1>
+<p className="whyThisTea">We would recommend our vegetable tea. Enjoy a tea full of antioxidants and a better night’s sleep. This is a very good choice for those whose bodies are in need of a little TLC, we add a hug to every cup!</p>
+
+<h1 className="recommendedTea">Camomile tea</h1>
+<p className="whyThisTea">We would recommend our camomile tea. De-stress from life and rest assured that we add a dash of peace to every cup!</p>
+
+
+
+
+
+</header>
