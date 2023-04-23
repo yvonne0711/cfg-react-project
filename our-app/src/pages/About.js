a15a74b05df406064ed9afbc3d7d2a67d30f5831
VerityWoodcock <vmhknowles@gmail.com>
2023-04-23 22:58:41 +0000

    About button is working!!!!

--- our-app/src/pages/About.js
+++ our-app/src/pages/About.js
@@ -5,23 +5,22 @@ import React from 'react';
 /* attempt to get the button working but it just brings you to the home page and the address bar has the path name below */
 import { useNavigate } from "react-router-dom";
 
-function About() {
-  let navigate = useNavigate(); 
-  const routeChange = () =>{ 
-    let path = `./About.js`; 
-    navigate(path);
-  }
+const About = (props) => {
+  const navigate = useNavigate(); 
 
-    return ( <div><h1>About</h1>
+    return ( 
+    <>
+    <h1>About</h1>
     <p className="aboutText">Take our fun and easy quiz to find out what your new favourite bubble tea flavour is! You can use our app to choose the ideal bubble tea for  you by answering a few short questions about your taste preferences.  We have everything you want, whether you prefer it sweet, fruity, or creamy.</p>  
     <p>Try it now and treat yourself to a pleasantly refreshing drink!"</p>
-    <div>
-      <button className="cta-button" onClick={routeChange}>Start Quiz</button>
-    </div>
-    </div>
-    )
-  };
-
+    <hr />
+      <p>
+        <button className="cta-button" onClick={() => navigate("/Quiz")}>Start Quiz</button>
+        </p>
+        </>
+    );
+};
+  
   export default About;
 
   /* previous code
