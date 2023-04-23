0c0c12a350e8c4829128895df79b53409493076e
VerityWoodcock <vmhknowles@gmail.com>
2023-04-23 22:29:05 +0000

    Attempting button on about
    Button can be clicked but unfortunately brings you to the home page :(

--- our-app/src/pages/About.js
+++ our-app/src/pages/About.js
@@ -1,13 +1,35 @@
 import './QuizExtra.css';
+import React from 'react';
 
-const About = () => {
-    return <> <h1>About</h1>
-    <p className="aboutText">Take our fun and easy quiz to find out what your new favourite bubble  tea flavour is! You can use our app to choose the ideal bubble tea for  you by answering a few short questions about your taste preferences.  We have everything you want, whether you prefer it sweet, fruity, or creamy.</p>  
+
+/* attempt to get the button working but it just brings you to the home page and the address bar has the path name below */
+import { useNavigate } from "react-router-dom";
+
+function About() {
+  let navigate = useNavigate(); 
+  const routeChange = () =>{ 
+    let path = `./About.js`; 
+    navigate(path);
+  }
+
+    return ( <div><h1>About</h1>
+    <p className="aboutText">Take our fun and easy quiz to find out what your new favourite bubble tea flavour is! You can use our app to choose the ideal bubble tea for  you by answering a few short questions about your taste preferences.  We have everything you want, whether you prefer it sweet, fruity, or creamy.</p>  
     <p>Try it now and treat yourself to a pleasantly refreshing drink!"</p>
     <div>
-              <button className="cta-button">Start Quiz</button>
-            </div>
-    </>
+      <button className="cta-button" onClick={routeChange}>Start Quiz</button>
+    </div>
+    </div>
+    )
   };
-  
+
   export default About;
+
+  /* previous code
+  const About = () => {
+    return <> <h1>About</h1>
+    <p className="aboutText">Take our fun and easy quiz to find out what your new favourite bubble  tea flavour is! You can use our app to choose the ideal bubble tea for  you by answering a few short questions about your taste preferences.  We have everything you want, whether you prefer it sweet, fruity, or creamy.</p>  
+                  <button className="cta-button">Start Quiz</button>
+            </div>
+    </>  
+    export default About;
+    */
