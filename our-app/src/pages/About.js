e714b0be9ac0508caad13b031c39e57e267f30be
VerityWoodcock <vmhknowles@gmail.com>
2023-04-23 14:28:37 +0000

    Update About.js
    Button from home page used for quiz button in about page

--- our-app/src/pages/About.js
+++ our-app/src/pages/About.js
@@ -4,7 +4,9 @@ const About = () => {
     return <> <h1>About</h1>
     <p className="aboutText">Take our fun and easy quiz to find out what your new favourite bubble  tea flavour is! You can use our app to choose the ideal bubble tea for  you by answering a few short questions about your taste preferences.  We have everything you want, whether you prefer it sweet, fruity, or creamy.</p>  
     <p>Try it now and treat yourself to a pleasantly refreshing drink!"</p>
-    <button className="startQuiz">Start quiz</button>
+    <div>
+              <button className="cta-button">Start Quiz</button>
+            </div>
     </>
   };
   
